28973871608f1bf419df5ac51fa9fc370d012942
Guillaume Julien <guillaume.julien@bjss.com>
2022-04-13 13:59:34 +0100

    removing dead code

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -17,17 +17,6 @@ export class HeroesComponent implements OnInit {
   }
 
 
-  selectedHero?: Hero;
-  onSelect(hero: Hero): void {
-    if (this.selectedHero === hero) {
-      delete this.selectedHero
-      this.messageService.clear()
-    } else {
-      this.selectedHero=hero;
-      this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
-    }
-  }
-
   getHeroes(): void {
     this.heroService.getHeroes()
     .subscribe(heroes => this.heroes = heroes)
