8a24769aafa27d970f3967ac6e844b02bcf4d807
Thegajout <gjuliens10@gmail.com>
2022-08-30 22:24:07 +0100

    refactor

--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -11,7 +11,7 @@ export class MessageService {
   constructor(private store: Store<State>) {}
 
   get messages$(): Observable<string[]> {
-    return this.store.select('messages').pipe(map(messages => messages.messages));
+    return this.store.select('messages').pipe(map(store => store.messages));
   }
 
   add(message: string) {
